2e226307c486b589418340ebc1cdca04288c0237
agehcx <chega.malee@gmail.com>
2024-04-09 17:41:13 +0700

    test app

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,34 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a StackPane as the root node
+        StackPane root = new StackPane();
+
+        // Set the background color of the root node to black
+        root.setStyle("-fx-background-color: black;");
+
+        // Create a Scene with the root node
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+
+        // Set the scene to the primary stage
+        primaryStage.setScene(scene);
+
+        // Set the title of the window
+        primaryStage.setTitle("Black Screen");
+
+        // Show the window
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
