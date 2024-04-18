bb7d3019377cec9972029589ced8e0cb0052ac4f
Aftermin <a.ftermin2@gmial.com>
2024-04-18 21:54:55 +0700

    edit movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,83 +1,96 @@
 package main;
 
-import javax.swing.*;
-import java.awt.*;
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends Pane {
     private static GamePanel instance;
     final int originalTitleSize = 16;
     final int scale = 3;
 
-    final int titleSize = originalTitleSize*scale;
+    final int titleSize = originalTitleSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = titleSize*maxScreenCol;
-    final int screenHeight = titleSize*maxScreenRow;
+    final int screenWidth = titleSize * maxScreenCol;
+    final int screenHeight = titleSize * maxScreenRow;
 
-    int FPS = 60;
-    KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
     int playerX = 100;
     int playerY = 100;
     int playerSpeed = 4;
-    public GamePanel(){
-        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
-        this.setBackground(Color.black);
-        this.setDoubleBuffered(true);
-        this.addKeyListener(keyH);
-        this.setFocusable(true);
+
+    private boolean upPressed = false;
+    private boolean downPressed = false;
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+
+    public GamePanel() {
+        this.setPrefSize(screenWidth, screenHeight);
+        this.setStyle("-fx-background-color: black;");
+        this.setFocusTraversable(true);
+
+        this.setOnKeyPressed(event -> {
+            KeyCode code = event.getCode();
+            if (code == KeyCode.UP) {
+                upPressed = true;
+            } else if (code == KeyCode.DOWN) {
+                downPressed = true;
+            } else if (code == KeyCode.LEFT) {
+                leftPressed = true;
+            } else if (code == KeyCode.RIGHT) {
+                rightPressed = true;
+            }
+        });
+
+        this.setOnKeyReleased(event -> {
+            KeyCode code = event.getCode();
+            if (code == KeyCode.UP) {
+                upPressed = false;
+            } else if (code == KeyCode.DOWN) {
+                downPressed = false;
+            } else if (code == KeyCode.LEFT) {
+                leftPressed = false;
+            } else if (code == KeyCode.RIGHT) {
+                rightPressed = false;
+            }
+        });
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+                repaint();
+            }
+        }.start();
+    }
+
+    private void update() {
+        if (upPressed) {
+            playerY -= playerSpeed;
+        } else if (downPressed) {
+            playerY += playerSpeed;
+        } else if (leftPressed) {
+            playerX -= playerSpeed;
+        } else if (rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    private void repaint() {
+        Canvas canvas = new Canvas(screenWidth, screenHeight);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(playerX, playerY, titleSize, titleSize);
+        getChildren().setAll(canvas);
     }
 
     public static GamePanel getInstance() {
         if (instance == null) {
             return new GamePanel();
         }
-
         return instance;
-    }
-
-    public void startGameThread(){
-        gameThread=new Thread(this);
-        gameThread.start();
-    }
-
-    @Override
-    public void run(){
-        double drawInterval = (double) 1000000000/FPS;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        while(gameThread!=null) {
-            currentTime = System.nanoTime();
-            delta += (currentTime-lastTime)/drawInterval;
-            lastTime=currentTime;
-            if(delta>=1){
-                update();
-                repaint();
-                delta--;
-            }
-        }
-    }
-    public void update(){
-        if(keyH.upPressed){
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed){
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed){
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed){
-            playerX += playerSpeed;
-        }
-    }
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.red);
-        g2.fillRect(playerX,playerY,titleSize,titleSize);
-        g2.dispose();
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,32 +6,29 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         StackPane root = new StackPane();
-
         root.setStyle("-fx-background-color: black;");
+
+        // Create a GamePanel instance and add it to the root node
+        GamePanel gamePanel = new GamePanel();
+        root.getChildren().add(gamePanel);
+
         // Create a Scene with the root node
-        // Scene scene = new Scene(root, 800, 600, Color.BLACK);
-        GamePanel gamePanel= new GamePanel();
-        Pane pane = new Pane();
-
-        primaryStage.setResizable(false);
-
-        primaryStage.centerOnScreen();
-
-        Scene scene = new Scene(pane, GamePanel.getInstance().getHeight(), GamePanel.getInstance().getWidth(),Color.BLACK);
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
 
         // Set the scene to the primary stage
-        Scene scene = new Scene(root, 800, 600, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Mafia Hatyai Progmeth");
+        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-
     }
 }
